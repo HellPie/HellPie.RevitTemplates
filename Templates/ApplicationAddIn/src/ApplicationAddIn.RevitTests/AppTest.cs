6f4564b9996a74e955f71b05d8364871de1dd9d8
Diego Rossi <git@hellpie.dev>
2020-11-12 15:52:38 +0100

    Add NUnit project for Revit using the Geberit runner

---
+++ Templates/ApplicationAddIn/src/ApplicationAddIn.RevitTests/AppTest.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using Autodesk.Revit.UI;
+using NUnit.Framework;
+
+namespace ApplicationAddIn.RevitTests {
+    public class AppTest {
+        [SetUp]
+        public void RunBeforeTest() {
+            Console.WriteLine($"Begin testing in {GetType().Name}");
+        }
+
+        [TearDown]
+        public void RunAfterTest() {
+            Console.WriteLine($"Finished testing in {GetType().Name}");
+        }
+
+        [Test]
+        public void PassTest() {
+            Assert.True(true);
+        }
+
+        [Test]
+        public void FailTest() {
+            Assert.True(false, "This test is expected to fail.");
+        }
+    }
+}
