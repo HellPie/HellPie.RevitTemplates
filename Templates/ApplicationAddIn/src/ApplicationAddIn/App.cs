cb097bd153bc2d3d4cee14aa6157ddb660efdc6a
Diego Rossi <git@hellpie.dev>
2020-11-12 10:40:39 +0100

    Add partial template for a Revit application

---
+++ Templates/ApplicationAddIn/src/ApplicationAddIn/App.cs
@@ -0,0 +1,17 @@
+﻿using System.Diagnostics.CodeAnalysis;
+using Autodesk.Revit.UI;
+
+namespace HellPie.RevitTemplates.Templates.ApplicationAddIn.src.ApplicationAddIn {
+    [SuppressMessage("ReSharper", "ClassNeverInstantiated.Global")]
+    internal class App : IExternalApplication {
+        /// <inheritdoc />
+        public Result OnStartup(UIControlledApplication application) {
+            return Result.Succeeded;
+        }
+
+        /// <inheritdoc />
+        public Result OnShutdown(UIControlledApplication application) {
+            return Result.Succeeded;
+        }
+    }
+}
