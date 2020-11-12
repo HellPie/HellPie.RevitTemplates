5e827f83f3116e4829d8f9e169ec90a434e776bd
Diego Rossi <git@hellpie.dev>
2020-11-12 16:06:34 +0100

    Add NUnit project for non-Revit tests

---
+++ Templates/ApplicationAddIn/src/ApplicationAddIn.Tests/SampleTest.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using NUnit.Framework;
+
+namespace ApplicationAddIn.Tests {
+    public class SampleTest {
+        [SetUp]
+        public void RunBeforeTest() {
+            Console.WriteLine($"Begin testing in {GetType().Name}");
+        }
+
+        [TearDown]
+        public void RunAfterTest() {
+            Console.WriteLine($"Finished testing in {GetType().Name}");
+        }
+
+        [Test]
+        public void PassTest() {
+            Assert.True(true);
+        }
+
+        [Test]
+        public void FailTest() {
+            Assert.True(false, "This test is expected to fail.");
+        }
+    }
+}
